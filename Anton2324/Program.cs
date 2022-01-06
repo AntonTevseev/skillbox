a181ceffc161658cc299ba0e629c293bd4fa08ff
AntonTevseev <91051811+AntonTevseev@users.noreply.github.com>
2022-01-06 15:32:57 +0200

    Initial commit

---
+++ Anton2324/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+using System.Threading;
+namespace xuy
+{
+    class Program
+    {
+        static void app(string N,string A)
+        {
+            string temp = N;
+            N = A;
+            A = temp;
+            Write($"{N} {A}");
+        }
+        static void Main(string[]ages)
+        {
+            string N = ReadLine();  
+            string A = ReadLine();
+            app(N,A);    
+        }
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+using System.Threading;
+namespace xuy
+{
+    class Program
+    {
+        static void Split()
+        {
+            string s = ReadLine();
+            string[] split = s.Split(" ");
+            foreach (var splits in split)
+            {
+                WriteLine(splits);
+            }
+
+        }
+        static void Main(string[] ages)
+        {
+            Split();
+        }
+    }
+}
+
