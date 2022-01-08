255c23bc279d2b68ae884523f97e6f475ef3df18
AntonTevseev <91051811+AntonTevseev@users.noreply.github.com>
2022-01-08 14:50:32 +0200

    Create .dtbcache.v2

---
+++ Anton2324.3.0/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+using System.Threading;
+
+namespace xuy
+{
+    class Program
+    {
+       
+        static void Main(string[]ages)
+        {
+            static void sss(string ss)
+            {
+                string[] Split = StringSplit(ss);
+                string revers="";
+
+                for(int i=Split.Length-1; i>=0;i--)
+                {
+                    revers=revers+Split[i]+" ";
+                }
+                WriteLine(revers);
+            }
+            
+            static string[] StringSplit(string s)
+            {
+                
+                string[] arr = s.Split(" ");
+                return arr;
+            }
+            sss(ReadLine());
+
+            
+            
+        }
+    }
+}
